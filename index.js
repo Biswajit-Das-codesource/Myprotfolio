c49b2148edbe0a952070ad18cfd4e0adedd726a7
biswajit das <biswajit9348das@gmail.com>
2024-09-09 15:36:27 +0530

    1st

---
+++ index.js
@@ -0,0 +1,23 @@
+gsap.from(".page-1-text-part h1",{
+  opacity:0,
+  duration:1,
+  y:-20
+})
+gsap.from(".page-1-text-part p",{
+  opacity:0,
+  duration:1,
+  delay:1,
+  y:"20px"
+})
+gsap.from(".cv",{
+  opacity:0,
+  duration:1,
+  delay:2,
+  y:"20px"
+})
+
+gsap.from(".page-1-image-part",{
+  opacity:0,
+  duration:1,
+  x:"20px"
+})
