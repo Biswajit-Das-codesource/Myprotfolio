153192bb9d55afcbd65446a36ff959ef38f82e32
biswajit das <biswajit9348das@gmail.com>
2025-03-07 21:30:19 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <div className=' bg-zinc-900'>
-    <div className=''>
+    <div className='min-h-screen'>
       <Navbar/>
       <Intro/>
       {/* <ProjectsSection/>
